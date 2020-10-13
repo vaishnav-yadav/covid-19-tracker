f1862c2ab97123adea1bd0c5962435ff3a191bad
Vaishnav Yadav <70229716+vaishnav-yadav@users.noreply.github.com>
2020-10-14 02:08:44 +0530

    implemented : fetching the countries name which are affected of corona in drop down through an api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect } from "react";
 import "./App.css";
 import { FormControl, MenuItem, Select } from "@material-ui/core";
 function App() {
-  const [countries, setCountries] = useState(["UK", "USA", "INDIA"]);
+  const [countries, setCountries] = useState([]);
+  
+  useEffect(() => {
+    //run once when app loads 
+    
+    //async -> send a request , wait for it , do something with the request 
+    const getCountriesData = async() =>{
+      await fetch('https://disease.sh/v3/covid-19/countries').then((response)=>response.json()).then((data)=>{
+        const countries = data.map((country)=>({
+          //returning object items of each  country:-
+          name : country.country, // United States
+          value: country.countryInfo.iso2 //USA
+        }));
+        setCountries(countries);
+
+      });
+    };
+   
+    getCountriesData(); //calling the getCountriesData()func , so every time app loads this func will run and gonna fetch the latest covid 19 data 
+      
+    
+  }, [])
+
   return (
     <div className="app">
       <div className="app__header">
@@ -11,7 +33,7 @@ function App() {
           <Select variant="outlined" value="abc">
             {/* loop through all the countries and show a dropdwon list of options  */}
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
 
             {/* 
