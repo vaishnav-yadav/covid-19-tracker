584bc43a4612b051801e93975d4940e87ca046ab
Vaishnav Yadav <70229716+vaishnav-yadav@users.noreply.github.com>
2020-10-14 01:42:14 +0530

    initialised useState hooks for all the countries in drop down menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { FormControl, MenuItem, Select } from "@material-ui/core";
 function App() {
+  const [countries, setCountries] = useState(["UK", "USA", "INDIA"]);
   return (
     <div className="app">
       <div className="app__header">
@@ -9,11 +10,13 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {/* loop through all the countries and show a dropdwon list of options  */}
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
 
+            {/* 
             <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-           
+            <MenuItem value="worldwide">Worldwide</MenuItem> */}
           </Select>
         </FormControl>
       </div>
