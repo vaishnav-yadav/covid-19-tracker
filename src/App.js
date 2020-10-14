7f806fa7cd1c82638f1acca76ce7b3acee8952e2
Vaishnav Yadav <70229716+vaishnav-yadav@users.noreply.github.com>
2020-10-14 12:01:36 +0530

    added app__left ,app__right div in app.js

--- src/App.js
+++ src/App.js
@@ -1,81 +1,85 @@
-import React, { useState ,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { FormControl, MenuItem, Select } from "@material-ui/core";
-import InfoBox from './Infobox';
-
+import { Card, CardContent, FormControl, MenuItem, Select } from "@material-ui/core";
+import InfoBox from "./Infobox";
+import Map from "./Map";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  //by default selected country will be worldwide  
-  const [country, setCountry] =useState('Worldwide');
-  
-  useEffect(() => {
-    //run once when app loads 
-    
-    //async -> send a request , wait for it , do something with the request 
-    const getCountriesData = async() =>{
-      await fetch('https://disease.sh/v3/covid-19/countries').then((response)=>response.json()).then((data)=>{
-        const countries = data.map((country)=>({
-          //returning object items of each  country:-
-          name : country.country, // United States country name 
-          value: country.countryInfo.iso2 //USA country code 
-        }));
-        setCountries(countries);
+  //by default selected country will be worldwide
+  const [country, setCountry] = useState("Worldwide");
 
-      });
+  useEffect(() => {
+    //run once when app loads
+
+    //async -> send a request , wait for it , do something with the request
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            //returning object items of each  country:-
+            name: country.country, // United States country name
+            value: country.countryInfo.iso2, //USA country code
+          }));
+          setCountries(countries);
+        });
     };
-   
-    getCountriesData(); //calling the getCountriesData()func , so every time app loads this func will run and gonna fetch the latest covid 19 data 
-      
-    
+
+    getCountriesData(); //calling the getCountriesData()func , so every time app loads this func will run and gonna fetch the latest covid 19 data
   }, []);
 
-
-  const onCountryChange = (event) =>{
+  const onCountryChange = (event) => {
     const countryCode = event.target.value;
     console.log(countryCode);
     setCountry(countryCode);
-  }
+  };
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onChange={onCountryChange}>
-            {/* loop through all the countries and show a dropdwon list of options  */}
-            <MenuItem value="Worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              {/* loop through all the countries and show a dropdwon list of options  */}
+              <MenuItem value="Worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={12121} total={3000} />
+          <InfoBox title="Recovered" cases={12121} total={200} />
+          <InfoBox title="Deaths" cases={12121} total={20000} />
+        </div>
 
-          </Select>
-        </FormControl>
+        {/* Header */}
+        {/* Title + select input drpodown field */}
+        {/* map */}
+        <Map />
       </div>
-      <div className ="app__stats">
-        <InfoBox title="Coronavirus Cases" cases={12121} total={3000}/>
-        <InfoBox title="Recovered" cases={12121} total={200}/>
-        <InfoBox title="Deaths" cases={12121} total={20000}/>
-
-      {/* info boxs title="corona virus cases "*/}
-      {/* info boxs title ="coronavirus recoveries"*/}
-      {/* info boxs */}
-      </div>
-
-      {/* Header */}
-      {/* Title + select input drpodown field */}
-     
-      {/* TABLE */}
-      {/* graph  */}
-      {/* map */}
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          {/* Table*/}
+          <h3>Worldwide new cases</h3>
+         { /*Graph */}
+        </CardContent>
+      </Card>
     </div>
   );
 }
 
 export default App;
 
-
-
+// response :-
 // {
 //   "updated": 1602654741421,
 //   "country": "Afghanistan",
